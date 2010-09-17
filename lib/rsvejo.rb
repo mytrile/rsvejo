dfc7788c340295629264a2fd7a373ba8b09e424b
mitkok <mitko.kostov@gmail.com>
2010-09-17 14:17:11 +0300

    some minor fixes

--- lib/rsvejo.rb
+++ lib/rsvejo.rb
@@ -7,11 +7,17 @@
 # LICENCE       : MIT
 # svejo.net API : http://groups.google.com/group/svejo/web/api 
 
-require 'rubygems'
-require 'httparty'
+begin
+  require 'httparty'
+rescue LoadError
+  require 'rubygems'
+  require 'httparty'
+end
 
-$KCODE ='u'
+$KCODE ='u' if RUBY_VERSION < '1.9'
 
+# The main RSvejo module
+# Usage: See the README.md
 module RSvejo
 
   extend self
@@ -19,34 +25,43 @@ module RSvejo
   API_URL = 'http://svejo.net/public_api/'
   FORMAT  = 'xml'
 
+  # Returns 15 of the latest 'fresh' stories
   def fresh_stories
     get_data("stories/fresh")
   end
 
+  # Returns 15 of the latest 'green' stories'
   def green_stories
     get_data("stories/green")
   end
 
+  # Returns 15 of the latest stories of the specified user
   def voted_by(name)
     get_data("user/#{name}/voted")
   end
 
+  # Returns 15 of the latest stories published by the specified user
+  # @param [String] -  name of the user
   def published_by(name)
     get_data("user/#{name}/published")
   end
 
+  # Returns 15 of the latest commented stories by the specified user
   def commented_by(name)
     get_data("user/#{name}/commented")
   end
 
+  # Returns 15 of the latest stories specified by the given tag
   def get_by_tag(tag)
     get_data("tag/#{tag}/new")
   end
 
+  # Returns 15 of the latest stories about the specified site
   def get_by_site(site)
     get_data("site/#{site}/new")
   end
 
+  # Returns 15 of the latest comments about the specified site
   def comments_for(url)
     get_data("comments/show/#{FORMAT}?url=#{url}")
   end
